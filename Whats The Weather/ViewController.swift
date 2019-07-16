920454cebb2c3987cf7e11249825eadac1ef2869
Solayman Rana <solaymanrana@Solaymans-MacBook-Pro.local>
2019-07-16 21:49:39 +0600

    Initial Commit

---
+++ Whats The Weather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Whats The Weather
+//
+//  Created by Solayman Rana on 16/7/19.
+//  Copyright © 2019 Solayman Rana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Whats The WeatherTests/Whats_The_WeatherTests.swift
@@ -0,0 +1,34 @@
+//
+//  Whats_The_WeatherTests.swift
+//  Whats The WeatherTests
+//
+//  Created by Solayman Rana on 16/7/19.
+//  Copyright © 2019 Solayman Rana. All rights reserved.
+//
+
+import XCTest
+@testable import Whats_The_Weather
+
+class Whats_The_WeatherTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
