61fd8830cb3a6083ed314b9143a938f3ac8edb3d
Solayman Rana <solaymanrana@Solaymans-MacBook-Pro.local>
2019-07-17 01:33:46 +0600

    first commit

--- Whats The Weather/ViewController.swift
+++ Whats The Weather/ViewController.swift
@@ -10,11 +10,81 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var cityTextField: UITextField!
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    @IBAction func getWeather(_ sender: Any) {
+        
+        if let url = URL (string: "https://www.weather-forecast.com/locations/"+cityTextField.text!.replacingOccurrences(of: " ", with: "-")+"/forecasts/latest") {
+            let request = NSMutableURLRequest(url: url)
+        let task = URLSession.shared.dataTask(with: request as URLRequest){
+            
+            data, response, error in
+            
+            var message = ""
+            
+            if error != nil {
+                
+                print (error)
+            } else {
+                
+                if let unWrappedData = data {
+                    
+                    let dataString = NSString (data: unWrappedData, encoding:  String.Encoding.utf8.rawValue)
+                    var stringSaparator = "Dhaka Weather Today </h2>(1&ndash;3 days)</span><p class=\"b-forecast__table-description-content\"><span class=\"phrase\">"
+                    
+                    if let contentArray = dataString?.components(separatedBy: stringSaparator) {
+                        if contentArray.count > 1 {
+                            
+                            stringSaparator = "</span>"
+                            
+                            let newContentArray = contentArray[1].components(separatedBy: stringSaparator)
+                            
+                            if newContentArray.count > 1 {
+                                
+                                message = newContentArray[0].replacingOccurrences(of: "&deg;", with: "°")
+                                print (message)
+                                
+                            }
+                            
+                        }
+                        
+                    }
+                    
+                }
+            }
+            
+            if message == "" {
+                
+                message = "The weather there couldn't be found. Please try again"
+            }
+            
+            DispatchQueue.main.async(execute: {
+                
+                self.resultLabel.text = message
+                
+            })
+            
+        }
+        
+        task.resume()
+        
+        } else {
+            
+            resultLabel.text = "The weather there couldn't be found. Please try again"
+        }
+        
     }
 
+        
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+       
 
 }
 
+}
